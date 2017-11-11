d84687f1ae881f0dea26b4a872e43b21f39f6fe3
Flinn <flinn@actsasflinn.com>
2017-11-11 17:18:00 -0500

    fix indenting

--- src/actions/widgets/create.cr
+++ src/actions/widgets/create.cr
@@ -1,11 +1,11 @@
 class Widgets::Create < BrowserAction
   action do
     WidgetForm.save(params) do |form, widget|
-     if widget
-       redirect to: Show.with(widget)
-     else
-       render NewPage, widget_form: form
-     end
+      if widget
+        redirect to: Show.with(widget)
+      else
+        render NewPage, widget_form: form
+      end
     end
   end
 end
--- src/actions/widgets/update.cr
+++ src/actions/widgets/update.cr
@@ -2,11 +2,11 @@ class Widgets::Update < BrowserAction
   action do
     widget = WidgetQuery.new.find(id)
     WidgetForm.update(widget, params) do |form, updated_widget|
-     if updated_widget
-       redirect to: Show.with(updated_widget)
-     else
-       render EditPage, widget_form: form, widget: updated_widget
-     end
+      if updated_widget
+        redirect to: Show.with(updated_widget)
+      else
+        render EditPage, widget_form: form, widget: updated_widget
+      end
     end
   end
 end
