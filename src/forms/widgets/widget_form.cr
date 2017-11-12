d20e77c0b550cf83ce2e7b170baf18f2454e13a7
Flinn <flinn@actsasflinn.com>
2017-11-11 19:41:49 -0500

    add validation of color

--- src/forms/widgets/widget_form.cr
+++ src/forms/widgets/widget_form.cr
@@ -1,5 +1,10 @@
+COLORS = %w[red orange yellow green blue purple violet indigo pink black brown gray white]
 class WidgetForm < Widget::BaseForm
   allow name
   allow color
   allow count
+
+  def prepare
+    validate_inclusions_of color, COLORS #, "should be one of: #{COLORS.join(", ")}"
+  end
 end
