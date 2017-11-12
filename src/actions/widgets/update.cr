dfa92a86b0f2716e6c19ccd773d8625473c3f756
Flinn <flinn@actsasflinn.com>
2017-11-11 19:41:32 -0500

    fix error handling

--- src/actions/widgets/update.cr
+++ src/actions/widgets/update.cr
@@ -2,7 +2,7 @@ class Widgets::Update < BrowserAction
   action do
     widget = WidgetQuery.new.find(id)
     WidgetForm.update(widget, params) do |form, updated_widget|
-      if updated_widget
+      if form.errors.empty?
         redirect to: Show.with(updated_widget)
       else
         render EditPage, widget_form: form, widget: updated_widget
